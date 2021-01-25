ba61c58a052b8895c1535d0fa55e765bd62cf5e9
aaa.com <aaa.com>
2021-01-25 19:27:06 +0800

    Config mapStateToProps

--- src/App.js
+++ src/App.js
@@ -2,28 +2,20 @@ import './App.css';
 import React from 'react';
 import TasksPage from './components/TasksPage';
 
-const mockTasks = [
-  {
-      id: 1,
-      title: 'Learn Redux',
-      description: 'The store, actions, and reducers, oh my!',
-      status: 'In Progress',
-  },
-  {
-      id: 2,
-      title: 'Peace on Earth',
-      description: 'No big deal.',
-      status: 'Unstarted'
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+  return {
+    tasks: state.tasks
   }
+}
 
-];
-
-function App() {
+function App(props) {
   return (
     <div className='main-content'>
-      <TasksPage tasks={mockTasks}></TasksPage>
+      <TasksPage tasks={props.tasks}></TasksPage>
     </div>
   );
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
